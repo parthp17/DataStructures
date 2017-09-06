3d0d0070c3dcf5b99a932b0f3a53523bed30f098
parthpandya17 <parth.pandya@sjsu.edu>
2017-09-06 13:24:05 -0700

    removing Design patterns

--- DataStructures/src/DesignPatterns/Main.java
+++
@@ -1,8 +0,0 @@
-package DesignPatterns;
-
-public class Main {
-	public static void main(String[] args) {
-		Pojo pojo = new Pojo.PojoBuilder().setA1("Parth").setA2("Mukeshkumar").setA3("Pandya").build();
-		System.out.println(pojo.toString());	
-	}
-}
